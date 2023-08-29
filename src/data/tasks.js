895c190fc95aa1dbee66fb5b73aba70522f8c3f9
WhoopyEv <WhoopyEv@gmail.com>
2023-08-29 08:25:37 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tareas = [
+    {
+        id:0,
+        title:'Tarea Uno', 
+        description:'Una tarea'
+    },
+    {
+        id:1,
+        title:'Tarea Dos', 
+        description:'Una tarea'
+    },
+    {
+        id:2,
+        title:'Tarea Tres', 
+        description:'Una tarea'
+    },
+]
